22368a6d785b40e33e7450cc25b829c9498c5bf2
C. Titus Brown <titus@idyll.org>
2017-10-29 09:28:37 -0700

    filter out null values

--- cmp-csv.py
+++ cmp-csv.py
@@ -2,16 +2,24 @@
 import csv
 import sys
 
+null_names = set(['[Blank]', 'na', 'null'])
+def filter_null(x):
+    for name in x:
+        if name in null_names:
+            yield ''
+        else:
+            yield name
+
 csv1 = csv.reader(open(sys.argv[1], 'rt'))
 csv2 = csv.reader(open(sys.argv[2], 'rt'))
 
 d1 = {}
 for row in csv1:
-    d1[row[0]] = row[1:]
+    d1[row[0]] = list(filter_null(row[1:]))
 
 d2 = {}
 for row in csv2:
-    d2[row[0]] = row[1:]
+    d2[row[0]] = list(filter_null(row[1:]))
 
 kk = set(d1.keys()).intersection(d2.keys())
 

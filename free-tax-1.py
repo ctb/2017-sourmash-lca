d7cc17b398c2836a0ea6a0a1f5a1d02dc77faef9
C. Titus Brown <titus@idyll.org>
2017-11-04 07:15:24 -0700

    free tax parsing

---
+++ free-tax-1.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python
+"""
+Load a genbank-free lineage, anchor with genbank.
+"""
+import sys
+import argparse
+import csv
+from collections import defaultdict, Counter
+import itertools
+import pprint
+import sourmash_lib
+import pickle
+
+sys.path.insert(0, '../2017-sourmash-revindex')
+import revindex_utils
+
+taxlist = ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus',
+           'species']
+null_names = set(['[Blank]', 'na', 'null'])
+
+
+_print_debug = False
+def debug(*args):
+    if _print_debug:
+        print(*args)
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('csv')
+    p.add_argument('revindex')
+    p.add_argument('lca_db')
+    p.add_argument('--scaled', default=10000, type=float)
+    p.add_argument('-d', '--debug', action='store_true')
+    args = p.parse_args()
+
+    if args.debug:
+        global _print_debug
+        _print_debug = True
+
+    ### parse spreadsheet
+    r = csv.reader(open(args.csv, 'rt'))
+    row_headers = ['identifier'] + taxlist
+
+    print('examining spreadsheet headers...', file=sys.stderr)
+    first_row = next(iter(r))
+
+    n_disagree = 0
+    for (column, value) in zip(row_headers, first_row):
+        if column.lower() != value.lower():
+            print('** assuming {} == {} in spreadsheet'.format(column, value),
+                  file=sys.stderr)
+            n_disagree += 1
+            if n_disagree > 2:
+                print('whoa, too many assumptions. are the headers right?',
+                      file=sys.stderr)
+                sys.exit(-1)
+
+    assignments = {}
+    for row in r:
+        lineage = list(zip(row_headers, row))
+
+        ident = lineage[0][1]
+        lineage = lineage[1:]
+
+        # clean lineage of null names
+        lineage = [(a,b) for (a,b) in lineage if b not in null_names]
+
+        assignments[ident] = tuple(lineage)
+
+    ## load revindex
+    print('loading reverse index:', args.revindex, file=sys.stderr)
+    custom_bins_ri = revindex_utils.HashvalRevindex(args.revindex)
+
+    # load the signatures associated with each revindex.
+    print('loading signatures for custom genomes...', file=sys.stderr)
+    sigids_to_sig = {}
+    ksizes = set()
+    for sigid, (filename, md5) in custom_bins_ri.sigid_to_siginfo.items():
+        sig = revindex_utils.get_sourmash_signature(filename, md5)
+
+        # downsample to specified scaled; this has the side effect of
+        # making sure they're all at the same scaled value!
+        sig.minhash = sig.minhash.downsample_scaled(args.scaled)
+
+        # check if there's more than one ksize...
+        ksizes.add(sig.minhash.ksize)
+        if len(ksizes) > 1:
+            raise Exception('ERROR: multiple ksizes!? {}'.format(str(ksizes)))
+            
+        if sig.name() in assignments:
+            sigids_to_sig[sigid] = sig
+        else:
+            debug('no assignment:', sig.name())
+
+    if not len(sigids_to_sig):
+        raise Exception('no custom genomes with assignments!?')
+
+    print('...found {} custom genomes in revindex with assignments!!'.format(len(sigids_to_sig)), file=sys.stderr)
+
+    ksize = ksizes.pop()
+    scaled = int(args.scaled)
+
+    ## now, connect the dots: hashvals to custom classifications
+    hashval_to_custom = defaultdict(list)
+    for hashval, sigids in custom_bins_ri.hashval_to_sigids.items():
+        for sigid in sigids:
+            sig = sigids_to_sig.get(sigid, None)
+            if sig:
+                assignment = assignments[sig.name()]
+                hashval_to_custom[hashval].append(assignment)
+
+    ## clean up with some indirection
+    next_lineage_index = 0
+    lineage_dict = {}
+    hashval_to_custom_idx = defaultdict(list)
+    
+    for hashval, assignments in hashval_to_custom.items():
+        for lineage_tuple in assignments:
+            idx = lineage_dict.get(lineage_tuple)
+            if idx is None:
+                idx = next_lineage_index
+                next_lineage_index += 1
+
+                lineage_dict[lineage_tuple] = idx
+
+            hashval_to_custom_idx[hashval].append(idx)
+
+    print(next(iter(hashval_to_custom_idx.items())))
+    print(next(iter(lineage_dict.items())))
+
+    # whew! done!! we can now go from a hashval to a custom assignment!!
+    print('saving to LCA DB v2: {}'.format(args.lca_db))
+    with open(args.lca_db, 'wb') as fp:
+        pickle.dump((ksize, scaled, hashval_to_custom), fp)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ free-tax-2.py
@@ -0,0 +1,318 @@
+#! /usr/bin/env python
+"""
+Load a genbank-free lineage, anchor with genbank.
+"""
+import sys
+import argparse
+import csv
+from collections import defaultdict, Counter
+import itertools
+import pprint
+import sourmash_lib
+import pickle
+
+DEFAULT_THRESHOLD=5                  # how many counts of a taxid at min
+
+taxlist = ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus',
+           'species']
+
+_print_debug = False
+def debug(*args):
+    if _print_debug:
+        print(*args)
+
+
+def build_tree(assignments, initial=None):
+    """
+    Builds a tree of dictionaries from lists of (rank, name) tuples
+    in 'assignments'.  This tree can then be used to find least common
+    ancestor agreements/confusion.
+    """
+    if initial is None:
+        tree = {}
+    else:
+        tree = initial
+
+    for assignment in assignments:
+        node = tree
+
+        for rank, name in assignment:
+            child = node.get((rank, name), {})
+            node[(rank, name)] = child
+
+            # shift -> down in tree
+            node = child
+
+    return tree
+
+
+def test_build_tree():
+    tree = build_tree([[('rank1', 'name1'), ('rank2', 'name2')]])
+    assert tree == { ('rank1', 'name1'): { ('rank2', 'name2') : {}} }
+
+
+def test_build_tree_2():
+    tree = build_tree([[('rank1', 'name1'), ('rank2', 'name2a')],
+                       [('rank1', 'name1'), ('rank2', 'name2b')],
+                      ])
+
+    assert tree == { ('rank1', 'name1'): { ('rank2', 'name2a') : {},
+                                           ('rank2', 'name2b') : {}} }
+
+
+def test_build_tree_3():
+    tree = build_tree([[('rank1', 'name1'), ('rank2', 'name2a')],
+                      ])
+
+    tree = build_tree([[('rank1', 'name1'), ('rank2', 'name2b')],
+                      ], tree)
+
+    assert tree == { ('rank1', 'name1'): { ('rank2', 'name2a') : {},
+                                           ('rank2', 'name2b') : {}} }
+
+
+def find_lca(tree):
+    """
+    Given a tree produced by 'find_tree', find the first node with multiple
+    children, OR the only leaf in the tree.  Return ((rank, name), reason),
+    where 'reason' is the number of children of the returned node, i.e.e
+    0 if it's a leaf and > 1 if it's an internal node.
+    """
+
+    node = tree
+    cur = ('root', 'root')
+    while 1:
+        if len(node) == 1:                # descend to only child
+            cur = next(iter(node.keys()))
+            node = node[cur]
+        elif len(node) == 0:              # at leaf; end
+            return cur, 0
+        else:                             # len(node) > 1 => confusion!!
+            return cur, len(node)
+
+
+def test_find_lca():
+    tree = build_tree([[('rank1', 'name1'), ('rank2', 'name2')]])
+    lca = find_lca(tree)
+
+    assert lca == (('rank2', 'name2'), 0)
+
+
+def test_find_lca_2():
+    tree = build_tree([[('rank1', 'name1'), ('rank2', 'name2a')],
+                       [('rank1', 'name1'), ('rank2', 'name2b')],
+                      ])
+    lca = find_lca(tree)
+
+    assert lca == (('rank1', 'name1'), 2)
+
+
+def build_reverse_tree(assignments, initial=None):
+    """
+    Builds a child -> parent dictionary (a reverse DAG) from lists of
+    (rank, name) tuples in 'assignments'.
+    """
+    if initial is None:
+        parents = {}
+    else:
+        parents = initial
+
+    for assignment in assignments:
+        last_node = ('root', 'root')
+        for rank, name in assignment:
+            parents[(rank, name)] = last_node
+            last_node = (rank, name)
+
+    return parents
+
+
+def test_build_reverse_tree():
+    parents = build_reverse_tree([[('rank1', 'name1'), ('rank2', 'name2')]])
+
+    print(parents)
+    assert parents == { ('rank2', 'name2'): ('rank1', 'name1'),
+                        ('rank1', 'name1'): ('root', 'root') }
+
+
+def test_build_reverse_tree_2():
+    parents = build_reverse_tree([[('rank1', 'name1'), ('rank2', 'name2a')],
+                                 [('rank1', 'name1'), ('rank2', 'name2b')],
+                                 ])
+
+    assert parents == { ('rank2', 'name2a'): ('rank1', 'name1'),
+                        ('rank2', 'name2b'): ('rank1', 'name1'),
+                        ('rank1', 'name1'): ('root', 'root') }
+
+
+def test_build_reverse_tree_3():
+    parents = build_reverse_tree([[('rank1', 'name1'), ('rank2', 'name2a')],
+                                 ])
+    parents = build_reverse_tree([[('rank1', 'name1'), ('rank2', 'name2b')],
+                                 ], parents)
+
+    assert parents == { ('rank2', 'name2a'): ('rank1', 'name1'),
+                        ('rank2', 'name2b'): ('rank1', 'name1'),
+                        ('rank1', 'name1'): ('root', 'root') }
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('--db', nargs='+', action='append')
+    p.add_argument('--query', nargs='+', action='append')
+    p.add_argument('--threshold', type=int, default=DEFAULT_THRESHOLD)
+    p.add_argument('-o', '--output', type=argparse.FileType('wt'),
+                   help='output CSV to this file instead of stdout')
+    #p.add_argument('-v', '--verbose', action='store_true')
+    p.add_argument('-d', '--debug', action='store_true')
+    args = p.parse_args()
+
+    if args.debug:
+        global _print_debug
+        _print_debug = True
+
+    ksize_vals = set()
+    scaled_vals = set()
+    dblist = []
+
+    # flatten --db and --query
+    args.db = [item for sublist in args.db for item in sublist]
+    args.query = [item for sublist in args.query for item in sublist]
+    
+    for db_name in args.db:
+        print(u'\r\033[K', end=u'', file=sys.stderr)
+        print('... loading database {}'.format(db_name), end='\r',
+              file=sys.stderr)
+        with open(db_name, 'rb') as fp:
+            (ksize, scaled, hashval_to_lineage) = pickle.load(fp)
+
+        ksize_vals.add(ksize)
+        if len(ksize_vals) > 1:
+            raise Exception('multiple ksizes, quitting')
+        scaled_vals.add(scaled)
+        if len(scaled_vals) > 1:
+            raise Exception('multiple scaled vals, quitting')
+
+        dblist.append(hashval_to_lineage)
+
+    print(u'\r\033[K', end=u'', file=sys.stderr)
+    print('loaded {} databases for LCA use.'.format(len(dblist)))
+        
+    # for each query, gather all the matches across databases, then
+    csvfp = csv.writer(sys.stdout)
+    if args.output:
+        print("outputting classifications to '{}'".format(args.output.name))
+        csvfp = csv.writer(args.output)
+    else:
+        print("outputting classifications to stdout")
+    csvfp.writerow(['ID'] + taxlist)
+
+    total_count = 0
+    for query_filename in args.query:
+        for query_sig in sourmash_lib.load_signatures(query_filename,
+                                                      ksize=ksize):
+            print(u'\r\033[K', end=u'', file=sys.stderr)
+            print('... classifying {}'.format(query_sig.name()), end='\r',
+                  file=sys.stderr)
+            debug('classifying', query_sig.name())
+            total_count += 1
+
+            # make sure we're looking at the same scaled value as database
+            query_sig.minhash = query_sig.minhash.downsample_scaled(scaled)
+
+            # gather assignments from across all the databases
+            these_assignments = defaultdict(list)
+            n_custom = 0
+            for hashval in query_sig.minhash.get_mins():
+                for hashval_to_custom in dblist:
+                    assignment = hashval_to_custom.get(hashval, [])
+                    if assignment:
+                        these_assignments[hashval].extend(assignment)
+                        n_custom += 1
+
+            # count number of assignments for each most-specific
+            check_counts = Counter()
+            for tuple_info in these_assignments.values():
+                last_tup = tuple(tuple_info[-1])
+                check_counts[last_tup] += 1
+
+            debug('n custom hashvals:', n_custom)
+            debug(pprint.pformat(check_counts.most_common()))
+
+            # now convert to trees -> do LCA & counts
+            counts = Counter()
+            parents = {}
+            for hashval in these_assignments:
+
+                # for each list of tuple_info [(rank, name), ...] build
+                # a tree that lets us discover least-common-ancestor.
+                tuple_info = these_assignments[hashval]
+                tree = build_tree(tuple_info)
+
+                # also update a tree that we can ascend from leaves -> parents
+                # for all assignments for all hashvals
+                parents = build_reverse_tree(tuple_info, parents)
+
+                # now find either a leaf or the first node with multiple
+                # children; that's our least-common-ancestor node.
+                lca, reason = find_lca(tree)
+                counts[lca] += 1
+
+            # ok, we now have the LCAs for each hashval, and their number
+            # of counts. Now sum across "significant" LCAs - those above
+            # threshold.
+
+            tree = {}
+            tree_counts = defaultdict(int)
+
+            debug(pprint.pformat(counts.most_common()))
+
+            n = 0
+            for lca, count in counts.most_common():
+                if count < args.threshold:
+                    break
+
+                n += 1
+
+                xx = []
+                parent = lca
+                while parent:
+                    xx.insert(0, parent)
+                    tree_counts[parent] += count
+                    parent = parents.get(parent)
+                debug(n, count, xx[1:])
+
+                # update tree with this set of assignments
+                build_tree([xx], tree)
+
+            if n > 1:
+                debug('XXX', n)
+
+            # now find LCA? or whatever.
+            lca, reason = find_lca(tree)
+            if reason == 0:               # leaf node
+                debug('END', lca)
+            else:                         # internal node
+                debug('MULTI', lca)
+
+            # backtrack to full lineage via parents
+            lineage = []
+            parent = lca
+            while parent != ('root', 'root'):
+                lineage.insert(0, parent)
+                parent = parents.get(parent)
+
+            # output!
+            row = [query_sig.name()]
+            for taxrank, (rank, name) in itertools.zip_longest(taxlist, lineage, fillvalue=('', '')):
+                if rank:
+                    assert taxrank == rank
+                row.append(name)
+
+            csvfp.writerow(row)
+
+    print(u'\r\033[K', end=u'', file=sys.stderr)
+    print('classified {} signatures total'.format(total_count), file=sys.stderr)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
